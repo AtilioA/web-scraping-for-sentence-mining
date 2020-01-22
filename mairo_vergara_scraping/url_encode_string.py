e319e2d85bd9b466479bec86609ec0ad8b657237
AtilioA <atiliodadalto@hotmail.com>
2020-01-22 17:50:07 -0300

    Add helper function and tests

---
+++ mairo_vergara_scraping/url_encode_string.py
@@ -0,0 +1,12 @@
+import re
+
+
+def url_encode_string(string):
+    """ "Encode" string to audio URL format of Mairo's.
+
+        Return encoded `string`.
+    """
+    
+    encodedString = string.replace(" ", "-")
+    encodedString = "".join(list(filter(lambda c: c not in "?.,!/;:’\'\"", encodedString)))
+    return re.sub('[^A-Za-z0-9\-]+', '', encodedString)
