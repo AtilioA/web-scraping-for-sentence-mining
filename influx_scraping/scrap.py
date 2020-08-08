07fb851cae0b3e5252a7f661d48a3c5c29ca8361
AtilioA <atiliodadalto@hotmail.com>
2020-08-08 12:45:36 -0300

    Fix (hopefully) inFlux blog posts scraping

--- influx_scraping/scrap.py
+++ influx_scraping/scrap.py
@@ -1,21 +1,26 @@
 import re
 import requests
-import itertools
 from bs4 import BeautifulSoup
 
 
 def format_english_sentence(sentence):
     """ Cleans a english sentence. Returns the new sentence """
 
-    # Replaces <em> tags with bold and underline
-    sentence = re.sub(r"<em>\s*", "<b><u>", sentence)
-    sentence = re.sub(r"(\W*)\s*<\/em>", r"</u></b>\1", sentence)
-    # sentence = re.sub(r"(<\/u><\/b>)(\w+)", r"\1 \2", sentence)
+    # Remove <strong> tags
+    sentence = re.sub(r"<\/?strong>", "", sentence)
 
-    # Removes extra whitespace
+    # Manage/remove extra whitespace
+    sentence = sentence.replace('&nbsp;', ' ')
+    sentence = sentence.replace("\xa0", " ")
     sentence = sentence.replace("  ", " ")
 
-    # Adds full stop if necessary
+    # Replace <u> tags with bold and underline
+    # sentence = re.sub(r"<strong>\s*", "<b><u>", sentence)
+    sentence = re.sub(r"<u>", "<b><u>", sentence)
+    sentence = re.sub(r"<\/u>", "</b></u>", sentence)
+    # sentence = re.sub(r"(<\/u><\/b>)(\w+)", r"\1 \2", sentence)
+
+    # Add full stop if necessary
     sentence = re.sub(r"(\w+)\s*\Z", r"\1.", sentence)
     sentence = re.sub(r"(<\/u><\/b>)\s*\Z", r"\1.", sentence)
 
@@ -25,72 +30,69 @@ def format_english_sentence(sentence):
 def format_portuguese_sentence(sentence):
     """ Cleans a portuguese sentence. Returns the new sentence """
 
-    # Removes <a> tags and extra whitespace
+    # Remove <a>, <em> tags and extra whitespace
     sentence = re.sub(r"\s\s+", ' ', sentence)
-    sentence = re.sub("""<a class="link_highlighted".*<em>""", "<b><u>", sentence)
-    sentence = sentence.replace("</em>", "</u></b>")
-    sentence = sentence.replace("</a>", '')
+    sentence = re.sub(r"<\/?em>", '', sentence)
+    sentence = re.sub(r"<\/?a>", '', sentence)
     # print(sentence)
 
-    # Replaces <strong> tags with bold and underline
-    sentence = re.sub(r"<strong>\s*", "<b><u>", sentence)
-    sentence = re.sub(r"(\W*)\s*<\/strong>", r"</u></b>\1", sentence)
-    sentence = re.sub(r"(<\/u><\/b>)(\w+)", r"\1 \2", sentence)
+    # Replace <u> tags with bold and underline
+    # sentence = re.sub(r"<strong>\s*", "<b><u>", sentence)
+    sentence = re.sub(r"<u>", "<b><u>", sentence)
+    sentence = re.sub(r"<\/u>", "</b></u>", sentence)
+    # sentence = re.sub(r"(<\/u><\/b>)(\w+)", r"\1 \2", sentence)
 
-    # Adds full stop if necessary
+    # Add full stop if necessary
     sentence = re.sub(r"(\w+)\s*\Z", r"\1.", sentence)
     sentence = re.sub(r"(<\/u><\/b>)\s*\Z", r"\1.", sentence)
 
     return sentence.strip()
 
 
-def crawl_page(targetURL):
-    """ Crawls a page looking for links to sentences """
-
-    pass
-
-
 def scrap_page(targetURL):
     """ Scraps a single URL for sentences, downloading audios """
 
+    # Extract post title from URL and use as .csv file name
     name = targetURL.split('/')[3]
     with open(f"csv/{name}.csv", "w+", encoding="utf8") as card:
+        # Headers for the GET request so it doesn't get easily rejected
         headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36'}
 
         req = requests.get(targetURL, headers=headers)
         if req.status_code == 200:
-            audiosFilenames = list()
             print('Successful GET request!')
+
+            # Initialize lists for later appending
+            englishSentences = list()
+            portugueseSentences = list()
+            audiosURLs = list()
 
             content = req.content
             html = BeautifulSoup(content, 'html.parser')
 
-            # Extracting english sentences
-            englishSentences = list()
-            div = html.find("div", class_="post-content")
-            divStrings = list(map(str, div))
-            sentences = [x for x in divStrings if re.search(r"<p style=\"text-align: justify;\">", x)]
-            print(sentences)
-            sentences = [x.split('</strong>') for x in sentences]
-            # englishSentences = [x[0] for x in sentences if "<u>" in str(x)]
-            # portugueseSentences = [x[1] for x in sentences if "<u>" in str(x)]
-            # print(portugueseSentences)
-            # print(len(portugueseSentences))
-            # print(englishSentences)
-            # print(len(englishSentences))
+            # Extract sentences
+            div = html.find("div", class_="post-content")  # Sentences are under this div
+            divStrings = ''.join(list(map(str, div)))
+            sentencesRegex = r"<p.*?>(.*?)<em>(.*?)<\/em>.*?<\/p>\s+<audio.*?src=\"(.*?)\".*?><\/audio>"
+            findSentences = re.findall(sentencesRegex, divStrings)
+            for matches in findSentences:
+                englishSentences.append(matches[0])
+                portugueseSentences.append(matches[1])
+                audiosURLs.append(matches[2])
+            del findSentences
 
-            # print(html)
-            # audios = re.findall(r"<audio src=.*\.mp3)(.*\n*<audio.*)*", str(html))
-            # print(audios)
-            # for audio in div.find_all("audio"):
-            #     print(audio)
+            audiosURLs = list(map(lambda url: f"https://blog.influx.com.br{url}", audiosURLs))  # Add domain to audio URLs
+            portugueseSentences = list(map(format_portuguese_sentence, portugueseSentences))  # Clean and format Portuguese
+            englishSentences = list(map(format_english_sentence, englishSentences))  # Clean and format English
 
-            # if len(englishSentences) != len(portugueseSentences) != len(audiosFilenames):
-            #     print(f"Lists don't have all the same length. Output may be compromised.\n{len(englishSentences)}, {len(portugueseSentences)}, {len(audiosFilenames)}")
+            if (portugueseSentences != englishSentences != audiosURLs):
+                print(f"""Lists don't have all the same length. Output may be compromised.
+- in '{name}':
+    ({len(englishSentences)} english sentences, {len(portugueseSentences)} portuguese sentences, {len(audiosURLs)} audio files.)""")
 
-            # cardInfos = [x for x in itertools.chain.from_iterable(itertools.zip_longest(englishSentences, portugueseSentences, audiosFilenames)) if x]
-            # for i in range(0, len(cardInfos) - 2, 3):
-            #     card.write(f"{cardInfos[i]}^{cardInfos[i + 1]}^[sound:{cardInfos[i + 2]}.mp3]^english_reverso\n")
+            cardInfos = zip(englishSentences, portugueseSentences, audiosURLs)
+            for sentence in cardInfos:
+                card.write(f"{sentence[0]}\t{sentence[1]}\t[sound:{sentence[2]}.mp3]\tenglish_influx\n")  # Use TAB as separator
         else:
             print('Failed GET request.')
 
